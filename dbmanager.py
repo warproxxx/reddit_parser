67343f7baa9bfb502a915efe85d086a3d1948b0a
Daniel Sapkota <daniel@quantorithm.com>
2019-02-25 17:06:14 +0545

    Works with comments

--- dbmanager.py
+++ dbmanager.py
@@ -1,8 +1,9 @@
 '''
 Set up the tables of the database
 '''
-import sqlite3 as sqlite
 import os
+import sqlite3 as sqlite
+
 
 class DBmanager():
     def __init__(self, dbname):
@@ -36,201 +37,35 @@ class DBmanager():
             os.remove(self.filepath)
 
     def create_tables(self):
-        '''Init empty tables to store threads, users and posts info''' 
+        '''Init empty tables to store comments and submissions''' 
         
-        self.cursor.execute("""create table threads(
-                            threadid TEXT   PRIMARY KEY, 
-                            title    TEXT, 
-                            forum     TEXT,
-                            root     TEXT
-                            )""")
-        
-        self.cursor.execute("""create table users(
-                            userid TEXT PRIMARY KEY, 
-                            username TEXT
-                            )""")
-        
-        self.cursor.execute("""create table posts(
+        self.cursor.execute("""create table comments(
                             postid TEXT     PRIMARY KEY, 
-                            user   TEXT     REFERENCES users(userid), 
-                            thread TEXT     REFERENCES threads(threadid),
-                            subject TEXT, 
-                            parent TEXT     REFERENCES users(userid), 
-                            date   TEXT, 
-                            text   TEXT
+                            created INT,
+                            author TEXT,
+                            parent TEXT,
+                            submission TEXT,
+                            body TEXT,
+                            score INT,
+                            upvotes INT,
+                            subreddit TEXT
                             )""")
         
-        self.cursor.execute('create index posts_user_idx on posts(user)')
-        self.cursor.execute('create index posts_thread_idx on posts(thread)')
-        self.cursor.execute('create index posts_parent_idx on posts(parent)')
         
-        
-    def insert_post(self, postid=None, user=None, thread=None, subject=None,
-                          parent=None, date=None, text=None):
+    def insert_comment(self, postid, created, author, parent, submission, body, score, upvotes, subreddit):
         self.cursor.execute("""INSERT INTO 
-                            posts(
+                            comments(
                             postid, 
-                            user, 
-                            thread,
-                            subject,
-                            parent, 
-                            date, 
-                            text) 
+                            created, 
+                            author,
+                            parent,
+                            submission, 
+                            body, 
+                            score,
+                            upvotes,
+                            subreddit
+                            ) 
                             VALUES 
-                            (?,?,?,?,?,?,?)"""
-                            , (postid, user, thread, subject, 
-                               parent, date, text))
-
-    def insert_thread(self, threadid, root, title, forum, ignore=False):
-        if not ignore:
-            self.cursor.execute("""INSERT INTO
-                                threads(threadid, title, forum, root)
-                                VALUES 
-                                (?,?,?,?)"""
-                                , (threadid, title, forum, root))
-        else:
-            self.cursor.execute("""INSERT IGNORE INTO
-                                threads(threadid, title, forum, root)
-                                VALUES 
-                                (?,?,?,?)"""
-                                , (threadid, title, forum, root))            
-
-    def insert_user(self, userid, username=None):
-        self.cursor.execute('''INSERT INTO
-                            users(userid, username)
-                            VALUES
-                            (?,?)'''
-                            ,(userid, username))
-
-    def set_threads(self):
-        '''Propagate thread information down the tree
-           Consider that the threadid is the postid of the root post
-           Use it when no other threadid provided
-           '''
-        
-        def propagate_thread(threadid, node):
-            '''Recursive propagation of threadid'''
-            
-            # Update info in its children
-            self.cursor.execute("""UPDATE posts SET thread=? 
-                                WHERE parent=?""", (threadid, node))
-
-            # Let children do the same with their children
-            cursor = self.cursor.execute("""SELECT postid FROM posts 
-                                         WHERE parent=?""", (node,))
-            
-            for row in cursor.fetchall():
-                print(node, row['postid'])
-                propagate_thread(threadid, row['postid'])
-                
-          
-        # Find roots      
-        cursor = self.cursor.execute("""SELECT postid, subject FROM posts 
-                                     WHERE parent IS NULL""")
-        # Tell all descendants that postid=root
-        for row in cursor.fetchall():
-            print("propagating from root...", row['postid'])
-
-            self.cursor.execute("""UPDATE posts SET thread=? WHERE postid=?""", 
-                                   (row['postid'],row['postid']))
-            self.insert_thread(row['postid'], row['postid'], row['subject'])
-            
-            propagate_thread(row['postid'], row['postid'])
-            
-  
-
-    def exists_thread(self, threadid):
-        '''
-        Returns True if table 'threads' contains row with threadid;
-        False otherwise
-        '''
-        u = self.cursor.execute("SELECT threadid FROM threads WHERE threadid=?" 
-                             , (threadid,)).fetchone()
-        if u is None:
-            return False
-        else:
-            return True
-        
-    def exists_user(self, userid):
-        '''
-        Returns True if table 'users' contains row with userid;
-        False otherwise
-        '''
-        u = self.cursor.execute("SELECT userid FROM users WHERE userid=?" 
-                             , (userid,)).fetchone()
-        if u is None:
-            return False
-        else:
-            return True
-        
-    def exists_post(self, postid):
-        '''
-        Returns True if table 'posts' contains row with userid;
-        False otherwise
-        '''
-        u = self.cursor.execute("SELECT postid FROM posts WHERE postid=?" 
-                             , (postid,)).fetchone()
-        if u is None:
-            return False
-        else:
-            return True
-        
-    def get_all_threads(self):
-        return self.cursor.execute("SELECT * FROM threads")
-    
-    def get_all_users(self):
-        return self.cursor.execute("SELECT * FROM users")
-
-    def get_all_posts(self):
-        return self.cursor.execute("SELECT * FROM posts")
-        
-    def get_threads_in_forum(self, forumid):
-        return self.cursor.execute('''SELECT * FROM threads 
-                                   WHERE forum=?'''
-                                   ,(forumid,))
-                                   
-    def get_users_in_thread(self, threadid):
-        return self.cursor.execute('''SELECT DISTINCT user FROM posts 
-                                   WHERE thread=?'''
-                                   ,(threadid,))
-
-    def get_posts_in_thread(self, threadid):
-        """
-        Return every post_id in thread
-        
-        Parameters
-        ----------
-        threadid : string
-          Thread id.
-        """
-        return self.cursor.execute("""SELECT * FROM posts 
-                           WHERE thread=? ORDER BY DATE ASC"""
-                           ,(threadid,))
-        
-    def get_user_threads(self, userid):
-        return self.cursor.execute("""SELECT DISTINCT thread FROM posts WHERE user=?"""
-                                   , (userid,))
-        
-    def get_username(self, userid):
-        return self.cursor.execute("""SELECT username FROM users 
-                                       WHERE userid=?"""
-                                    ,(userid,))
-         
-    def get_thread_title(self, threadid):
-        return self.cursor.execute("""SELECT title FROM threads 
-                                    WHERE threadid=?"""
-                                    ,(threadid,))
-    
-    def delete_thread(self, threadid):
-        self.cursor.execute("""DELETE FROM threads WHERE threadid=?"""
-                            , (threadid,))
-
-    def update_post_date(self, postid, date):
-        self.cursor.execute("""UPDATE posts SET date=? WHERE postid=?"""
-                            , (date, postid))
-                            
-if __name__=="__main__":
-    dbfile = "test.db"
-    db = DBmanager(dbfile)
-    db.insert_post(1, 1, 3, 4, None, 4, "test")
-    db.dbcommit()
+                            (?,?,?,?,?,?,?,?,?)"""
+                            , (postid, created, author, parent, 
+                               submission, body, score, upvotes, subreddit))
--- parser_reddit.py
+++ parser_reddit.py
@@ -9,126 +9,46 @@ import os
 import dbmanager as dbmanager
 from os.path import join
 import json
+from glob import glob
 
 def parse_reddit():
     '''
     Parse reddit from json files and store them in a SQLite database
     '''
-    filepath = os.path.dirname(os.path.abspath(__file__))
-    db = dbmanager.DBmanager(join('output', 'reddit.db'))
-    input_file = join(filepath, "input", "RC_2009-06")
-    
+    all_comments = glob('input/comments/*')
 
-    nposts = sum(1 for line in open(input_file, 'r'))
-    with open(input_file, 'r') as f:
+    for comment_file in all_comments:
+        db = dbmanager.DBmanager(join('output', 'reddit.db'))
+        input_file = comment_file
         
-        # Every line is a post        
-        nforums=0
-        for i, line in enumerate(f):
-                         
-            user= "anonymous"
-            date = None
-            parent = None
-            linkid = None
-            threadid = None
-            forum = None
+
+        nposts = sum(1 for line in open(input_file, 'r'))
+        with open(input_file, 'r') as f:
             
-            d = json.loads(line)
-            postid = d["name"] # t1_cnas8zv
-            text = d["body"]
-            user = d["author"]
-            threadid = d["link_id"] # t3_... (id of root link)
-            date = d["created_utc"] # seconds   
-            parent = d["parent_id"] # t3_ if first level comment; else t1_... 
-            forumid = d["subreddit_id"] #t5_...
-            forum = d["subreddit"] # showerthoughts
-            id_ = d["id"] # cnas8zv
-    
-            root = threadid
-    
-            try:
+            # Every line is a post        
+
+            for i, line in enumerate(f):
                 
-                if not db.exists_thread(threadid):
-    
-                    # Insert the root posts, even if we do not have its full info
-                    db.insert_post(postid=root, 
-                                   user="root", 
-                                   thread=threadid,
-                                   subject=None, 
-                                   parent=None, 
-                                   date=None, 
-                                   text=None)    
-                           
-                    db.insert_thread(threadid, root, None, forum)
-                    nforums += 1
-            except Exception as e:
-                pass
-    
-            if not db.exists_user(user):
-                username = user 
-                db.insert_user(userid=user, username=username)
-    
-            db.insert_post(postid=postid, 
-                           user=user, 
-                           thread=threadid,
-                           subject=None, 
-                           parent=parent, 
-                           date=date, 
-                           text=text)    
-                                                    
-            #1 000 000
-            if i%100000 == 0:
-                print("Processed", i, "/", nposts , 100.0*i/nposts, "posts/forum", 1.0*i/nforums)
-            if i%20000000 == 0:
-                print("Processed", i, "/", nposts , 100.0*i/nposts, "posts/forum", 1.0*i/nforums)
-                db.dbcommit()
-                print(".....................................")
+                d = json.loads(line)
+                idstr = d["name"] # t1_cnas8zv
+                created = d["created_utc"]
+                author = d["author"]
+                parent = d["parent_id"] # t3_ if first level comment; else t1_... 
+                submission = d["link_id"] # t3_... (id of root link)
+                body = d['body']
+                score = d['score']
+                upvotes = d['ups']
+                subreddit = d['subreddit']
         
-        db.dbcommit()
-        
-        # Delete threads where some post has a parent which is not in the database
-        # unconnected trees
-        threads = db.get_all_threads().fetchall()
-        nthreads = len(threads)
-        print("N threads:", len(threads))
-        print("N posts:", len(db.get_all_posts().fetchall()))
-        
-        complete_threads = 0
-        ok_posts = 0
-        
-        for thread in threads:
-            threadid = thread['threadid']
-            posts = db.get_posts_in_thread(threadid).fetchall()
+                db.insert_comment(idstr, created, author, parent, submission, body, score, upvotes, subreddit)    
+                                                        
+                if i%100000 == 0:
+                    print("Processed", i, "/", nposts , 100.0*i/nposts)
+                if i%20000000 == 0:
+                    print("Processed", i, "/", nposts , 100.0*i/nposts)
+                    db.dbcommit()
+                    print(".....................................")
             
-            for post in posts:
-                if post['postid'] == threadid: continue # skip root
-                if not db.exists_post(post['parent']):
-                    db.delete_thread(threadid)
-                    complete_threads -= 1
-                    break
-                else:
-                    ok_posts +=1
-                    
-            complete_threads +=1
-        
-        db.dbcommit()  
-
-        print("\nAfter cleaning:")
-        print("Complete threads", complete_threads, "/", nthreads)
-        print("Post with parent in db", ok_posts)
-        print("N threads final:", len(db.get_all_threads().fetchall()))
-        print("N posts final:", len(db.get_all_posts().fetchall()))          
-                  
-    
-        # Set (fake) date of root
-        # to one second before the first post
-        for thread in threads:
-            root = thread['root']
-            posts = db.get_posts_in_thread(root).fetchall()
-            date = int(posts[1]['date'])-1 # root is posts[0] since NULL is a 0
-            db.update_post_date(root, date)    
-            
-        db.dbcommit()
-        
+            db.dbcommit()
         
 parse_reddit()
